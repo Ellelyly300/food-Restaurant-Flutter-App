9a81cc36ee52550038ae973de64387533e4aa723
Nawinda Prayoonsak <nawinda.prayoonsak@m.swu.ac.th>
2024-05-04 00:33:26 +0700

    Moved from another repo

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:foodly_ui/constants.dart';
 import 'package:foodly_ui/screens/onboarding/onboarding_scrreen.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  // This widget is the root of your application. dd
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -25,7 +19,7 @@ class MyApp extends StatelessWidget {
         elevatedButtonTheme: ElevatedButtonThemeData(
           style: ElevatedButton.styleFrom(
             backgroundColor: primaryColor,
-            foregroundColor: Colors.white,
+            foregroundColor: Color.fromARGB(255, 255, 255, 255),
             minimumSize: const Size(double.infinity, 40),
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(8),
--- lib/screens/search/search_screen.dart
+++ lib/screens/search/search_screen.dart
@@ -66,14 +66,14 @@ class _SearchScreenState extends State<SearchScreen> {
                         : RestaurantInfoBigCard(
                             // Images are List<String>
                             images: demoBigImages..shuffle(),
-                            name: "McDonald's",
+                            name: "ไก่ทอด หาดหย่าย",
                             rating: 4.3,
                             numOfRating: 200,
                             deliveryTime: 25,
                             foodType: const [
-                              "Chinese",
-                              "American",
-                              "Deshi food"
+                              "น่อง",
+                              "สะโพก",
+                              "ปีก"
                             ],
                             press: () {},
                           ),
