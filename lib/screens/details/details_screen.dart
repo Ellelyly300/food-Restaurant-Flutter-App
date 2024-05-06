30e2a83a6c283d2153717b16bd1e17cace1175a6
TyKung123456 <smg.ttmt@gmail.com>
2024-05-06 19:42:13 +0700

    dd pls do next

--- lib/screens/details/details_screen.dart
+++ lib/screens/details/details_screen.dart
@@ -1,101 +1,46 @@
 import 'package:flutter/material.dart';
-import 'package:foodly_ui/demoData.dart';
+import 'package:flutter_svg/svg.dart';
+
 import '../../constants.dart';
-import 'package:foodly_ui/CartModel.dart';
-import 'package:provider/provider.dart';
+import '../search/search_screen.dart';
+import 'components/iteams.dart';
+import 'components/restaurrant_info.dart';
 
 class DetailsScreen extends StatelessWidget {
-  const DetailsScreen({Key? key}) : super(key: key);
+  const DetailsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        actions: [],
-      ),
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: restaurantMenu.keys.map((restaurantName) {
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: EdgeInsets.symmetric(
-                      horizontal: defaultPadding,
-                      vertical: defaultPadding / 2,
-                    ),
-                    child: Text(
-                      restaurantName,
-                      style: Theme.of(context).textTheme.headline6,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 8.0,
-                  ),
-                  ListView.builder(
-                    shrinkWrap: true,
-                    physics: NeverScrollableScrollPhysics(),
-                    itemCount: restaurantMenu[restaurantName]!.length,
-                    itemBuilder: (context, index) {
-                      final item = restaurantMenu[restaurantName]![index];
-                      return Padding(
-                        padding: EdgeInsets.symmetric(
-                          horizontal: defaultPadding,
-                          vertical: defaultPadding / 2,
-                        ),
-                        child: _buildItemCard(context, item),
-                      );
-                    },
-                  ),
-                ],
-              );
-            }).toList(),
+        actions: [
+          IconButton(
+            icon: SvgPicture.asset("assets/icons/share.svg"),
+            onPressed: () {},
           ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildItemCard(BuildContext context, Map<String, dynamic> item) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(16.0),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.5),
-            spreadRadius: 1,
-            blurRadius: 3,
-            offset: Offset(0, 2), // changes position of shadow
+          IconButton(
+            icon: SvgPicture.asset("assets/icons/search.svg"),
+            onPressed: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const SearchScreen(),
+              ),
+            ),
           ),
         ],
       ),
-      child: ListTile(
-        leading: Image.asset(
-          item["image"] ?? "No Image",
-          width: 80,
-          height: 80,
-          fit: BoxFit.cover,
+      body: const SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: defaultPadding / 2),
+              RestaurantInfo(),
+              SizedBox(height: defaultPadding),
+              Items(),
+            ],
+          ),
         ),
-        title: Text(
-          item["name"] ?? "No Title",
-          style: TextStyle(fontSize: 18),
-        ),
-        subtitle: Text(
-          item["location"] ?? "No Location",
-          style: TextStyle(fontSize: 14),
-        ),
-        onTap: () {
-          final cartItem = {
-            "name": item["name"] ?? "No Title",
-            "price": item["price"]?.toString() ?? "0.00",
-            "image": item["image"] ?? "No Image",
-          };
-          // Provider.of<CartModel>(context, listen: false)
-          //     .addItemToCart(cartItem);
-        },
       ),
     );
   }
